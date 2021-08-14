fe9286170910023e18cf37a16c7be7e70c57972a
Mfpass1992 <aleksanderburkowski@gmail.com>
2021-08-14 20:19:28 +0200

    Adding front and backend elements
    Search bar and genre listbox added

---
+++ Data/InitialData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesTestApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesTestApp.Data
+{
+    public class InitialData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context = new RazorPagesTestAppContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPagesTestAppContext>>()))
+            {
+                if (context.MovieModel.Any())
+                {
+                    return;
+                }
+
+                context.MovieModel.AddRange
+                    (
+                        new MovieModel
+                        {
+                            Title = "Whiplash",
+                            ReleaseDate = DateTime.Parse("2014-01-16"),
+                            Genre = "Psychological drama",
+                            Price = 10.99M
+                        },
+                        new MovieModel
+                        {
+                            Title = "GhostBusters",
+                            ReleaseDate = DateTime.Parse("1984-03-13"),
+                            Genre = "Comedy",
+                            Price = 8.99M
+                        },
+                        new MovieModel
+                        {
+                            Title = "Twelve years a Slave",
+                            ReleaseDate = DateTime.Parse("2013-08-30"),
+                            Genre = "Historical drama",
+                            Price = 15.99M
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesTestApp.Data;
 using RazorPagesTestApp.Models;
@@ -19,11 +20,31 @@ namespace RazorPagesTestApp.Pages
             _context = context;
         }
 
-        public IList<MovieModel> MovieModel { get;set; }
-
+        public IList<MovieModel> MovieModel { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string searchString { get; set; }
+        public SelectList genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string movieGenre { get; set; }
         public async Task OnGetAsync()
         {
-            MovieModel = await _context.MovieModel.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.MovieModel
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.MovieModel
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            MovieModel = await movies.ToListAsync();
         }
     }
 }
